8039605835716ed54cf0555ac6e6572348b49c2b
MeloQi <bdqi@cm-topsci.com>
2019-12-09 17:40:52 +0800

    add bits pos

--- utils/utils.go
+++ utils/utils.go
@@ -35,6 +35,49 @@ func LocalIP() string {
 	return ip
 }
 
+
+//BitsBuffer bits buffer
+type BitsBuffer struct {
+	iSize int
+	iData int
+	iMask uint8
+	pData []byte
+}
+
+func BitsInit(isize int, buffer []byte) *BitsBuffer {
+
+	bits := &BitsBuffer{
+		iSize: isize,
+		iData: 0,
+		iMask: 0x80,
+		pData: buffer,
+	}
+	if bits.pData == nil {
+		bits.pData = make([]byte, isize)
+	}
+	return bits
+}
+
+func BitsWrite(bits *BitsBuffer, count int, src uint64) *BitsBuffer {
+
+	for count > 0 {
+		count--
+		if ((src >> uint(count)) & 0x01) != 0 {
+			bits.pData[bits.iData] |= bits.iMask
+		} else {
+			bits.pData[bits.iData] &= ^bits.iMask
+		}
+		bits.iMask >>= 1
+		if bits.iMask == 0 {
+			bits.iData++
+			bits.iMask = 0x80
+		}
+	}
+
+	return bits
+}
+
+
 func MD5(str string) string {
 	encoder := md5.New()
 	encoder.Write([]byte(str))
